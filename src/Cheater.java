54ab30651c0605a74f8164aba08bb1ed08be1548
ZZooRM <annkoff@ya.ru>
2016-06-11 18:30:59 +0300

    Удалил говно комменты

--- src/Cheater.java
+++ src/Cheater.java
@@ -1,6 +1,3 @@
-/**
- * Created by user on 09.06.2016.
- */
 public class Cheater {
     private boolean typeCheater; // false - cheater is a man. true - cheater is a company.
     private boolean sellerOrBuyer; // false - cheater is a seller. true - cheater is a buyer.
